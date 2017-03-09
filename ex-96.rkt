e1fa9f65ee432261a0545c0151d96784887891cb
bgusach <bgusach@gmail.com>
2017-03-09 13:45:30 +0100

    ex 96 improved

--- ex-96.rkt
+++ ex-96.rkt
@@ -41,12 +41,13 @@
 ;   speed dx pixels/tick
 
 ; a Missile is a Posn
-; interepretation (make-posn x y): missile location
+; interepretation (make-posn x y): missile at (x, y)
 
 
 (define-struct aim [ufo tank])
 ; an Aim is a structure (make-aim sUFO Tank)
 ; interpretation: state of the ufo and the tank, prior to firing the missile
+
 
 (define-struct fired [ufo tank missile])
 ; a Fired is a structure (make-fired sUFO Tank Missile)
@@ -60,50 +61,75 @@
 
 
 ; SIGS -> Image
+; Adds TANK, UFO and possibly a MISSILE to the BACKGROUND
 (define (render-world world)
-  (place-image 
-    TANK
-    (tank-loc (tank-from-world world))
-    (- BACKGROUND-H TANK-H)
-    (place-image
-      MISSILE
-      ; This repetition is annoying: waiting for some kind of let...
-      (posn-x (missile-location world))
-      (posn-y (missile-location world))
-      (place-image
-        UFO
-        (posn-x (ufo-from-world world))
-        (posn-y (ufo-from-world world))
+  (cond
+    [(aim? world) (render-aim-world world)]
+    [(fired? world) (render-fired-world world)]))
+
+
+; make-aim -> Image
+; creates an image for an aim? data-structure
+(define (render-aim-world ws)
+  (render-ufo 
+    (aim-ufo ws)
+    (render-tank
+      (aim-tank ws)
+      (render-missile
+        ; Calculate missile position from tank position
+        (make-posn 
+          (tank-loc (aim-tank ws))
+          (- BACKGROUND-H TANK-H 15))
         BACKGROUND))))
 
 
-; SIGS -> make-posn
-(define (missile-location world)
-  (cond 
-    [(fired? world) (fired-missile world)]
-    [else (make-posn 
-            (tank-loc (aim-tank world))
-            (- BACKGROUND-H TANK-H MISSILE-H))]))
+; make-fired -> Image
+; creates an image for a world in status make-fired
+(define (render-fired-world ws)
+  (render-ufo 
+    (fired-ufo ws)
+    (render-tank
+      (fired-tank ws)
+      (render-missile
+        (fired-missile ws)
+        BACKGROUND))))
 
 
-; SIGS -> make-tank
-(define (tank-from-world world)
-  ; I hope there are more polymorphic ways of doing this... 
-  (cond 
-    [(fired? world) (fired-tank world)]
-    [else (aim-tank world)]))
+; Tank Image -> Image
+; adds a tank image to the passed image
+(define (render-tank tank img)
+  (place-image 
+    TANK
+    (tank-loc tank)
+    (- BACKGROUND-H TANK-H)
+    img))
 
-; SIGS -> sUFO
-(define (ufo-from-world world)
-  (cond
-    [(fired? world) (fired-ufo world)]
-    [else (aim-ufo world)]))
+
+; sUFO Image -> Image
+; adds a ufo image to the passed image
+(define (render-ufo ufo img)
+  (place-image
+    UFO
+    (posn-x ufo)
+    (posn-y ufo)
+    img))
+
+
+; Missile Image -> Image
+; adds a missile image to the passed image
+(define (render-missile mis img)
+  (place-image
+    MISSILE
+    (posn-x mis)
+    (posn-y mis)
+    img))
 
 
 (define (main world)
   (big-bang 
     world
     [to-draw render-world]))
+
 
 (main (make-aim (make-posn 30 70) (make-tank 200 0)))  ; still aiming
 (main (make-fired (make-posn 10 10) (make-tank 50 0) (make-posn 60 60))) ; already fired
