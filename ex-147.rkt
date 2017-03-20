0f4709557699b197cfe708b2c4fbe2a26700f464
bgusach <bgusach@gmail.com>
2017-03-20 12:26:34 +0100

    Ex. 148 renamed to 147 and completed

--- ex-147.rkt
+++ ex-147.rkt
@@ -7,10 +7,12 @@
 
 ; NEList-of-bools -> Boolean
 ; Returns whether all the elements of the list are true
+
 (check-expect (all-true (cons #true '())) #true)
 (check-expect (all-true (cons #false '())) #false)
 (check-expect (all-true (cons #true (cons #true '()))) #true)
 (check-expect (all-true (cons #true (cons #false '()))) #false)
+
 (define (all-true lob)
   (cond
     [(empty? (rest lob)) (first lob)]
@@ -19,6 +21,25 @@
        (first lob)
        (all-true (rest lob))
        )]))
+
+
+; NEList-of-bools -> Boolean
+; Returns whether at least one element of the list is #true
+
+(check-expect (one-true (cons #true '())) #true)
+(check-expect (one-true (cons #false '())) #false)
+(check-expect (one-true (cons #true (cons #true '()))) #true)
+(check-expect (one-true (cons #true (cons #false '()))) #true)
+
+(define (one-true lob)
+  (cond
+    [(empty? (rest lob)) (first lob)]
+    [else
+      (or
+        (first lob)
+        (one-true (rest lob))
+        )]))
+
 
 
 (require test-engine/racket-tests)
