c2cc78e0a543c1b2547e41aba3e34e5d0486a69d
bgusach <bgusach@gmail.com>
2017-04-20 13:45:27 +0200

    Ex. 195

---
+++ ex-195.rkt
@@ -0,0 +1,47 @@
+#lang htdp/bsl+
+(require 2htdp/batch-io)
+
+; A Dictionary is one of:
+; - '()
+; - (const String Dictionary)
+
+
+; ### Data Definitions
+; A Letter is one of the following 1Strings: 
+; – "a"
+; – ... 
+; – "z"
+; or, equivalently, a member? of this list: 
+(define LETTERS (explode "abcdefghijklmnopqrstuvwxyz"))
+
+
+; ### Functions
+
+; Letter Dictionary -> Number
+; Counts how many words start with letter in the dict
+(check-expect (starts-with# "a" (list "arrow" "available" "pig" "donkey")) 2)
+(check-expect (starts-with# "z" (list "arrow" "pig" "donkey")) 0)
+(define (starts-with# letter dict)
+  (cond
+    [(empty? dict) 0]
+    [else
+      (+
+        (if (starts-with? (first dict) letter) 1 0)
+        (starts-with# letter (rest dict))
+        )]))
+
+
+; String 1-String -> Boolean
+; Checks whether str strings with first
+(check-expect (starts-with? "lol" "l") #true)
+(check-expect (starts-with? "lol" "z") #false)
+(define (starts-with? str first)
+  (string=?
+    (string-ith str 0)
+    first
+    ))
+
+
+(require test-engine/racket-tests)
+(test)
+
