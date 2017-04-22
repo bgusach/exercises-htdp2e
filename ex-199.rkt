4d167520c7bd337042b555c580302d236a71ac23
bgusach <bgusach@gmail.com>
2017-04-22 09:49:43 +0200

    Added 199

--- ex-199.rkt
+++ ex-199.rkt
@@ -1,15 +1,39 @@
 #lang htdp/bsl+
 
-(require 2htdp/batch-io)
 (require 2htdp/itunes)
 
-; ### Constants
+
+; ### Data Definitions
+
+; A Date is a structure:
+;   (make-date N N N N N N)
+; interpretation An instance records six pieces of information:
+; year, 
+; month (between 1 and 12 inclusive), 
+; day (between 1 and 31), 
+; hour (between 0 and 23),
+; minute (between 0 and 59),
+; and second (also between 0 and 59).
+
+
+; A Track is a structure:
+;   (make-track String String String N N Date N Date)
+; interpretation An instance records in order: 
+; - the track's title, 
+; - artist, 
+; - album
+; - length in milliseconds, 
+; - position within the album
+; - the date it was added
+; - how often it has been played, 
+; - and the date when it was last played
+; Examples:
 (define 
   track1 
   (create-track
-    "Mi carro"
-    "Don Manuel Escobar"
-    "Manolo's greatest hits"
+    "Pacman in the dark"
+    "Johnny and the drunken squirrels"
+    "Once upon a time"
     (* 3 60 1000)
     1
     (create-date 2015 6 3 12 30 30)
@@ -20,21 +44,37 @@
 (define 
   track2
   (create-track
-    "Donde estará mi carro"
-    "Don Manuel Escobar"
-    "Manolo's greatest hits"
-    (* 3 60 1000)
-    1
-    (create-date 2015 6 3 12 30 30)
-    42
-    (create-date 2017 9 2 21 45 15)
+    "My little goat"
+    "Johnny and the drunken squirrels"
+    "Once upon a time"
+    (* 2 60 1000)
+    2
+    (create-date 2014 7 2 12 0 0)
+    93
+    (create-date 2017 9 3 21 0 0)
     ))
 
-; ### Data Definitions
+(define 
+  track3
+  (create-track
+    "Eating cookies after the dusk"
+    "Marieta Marieta"
+    "My last album"
+    (* 180 1000)
+    1
+    (create-date 2000 3 2 12 1 0)
+    600
+    (create-date 2017 9 3 21 0 0)
+    ))
+
+; A LTracks is one of:
+; - '()
+; - (cons Track LTracks)
+; Example:
+(define track-list (list track1 track2 track3))
+
 
 ; ### Functions
-track1
-track2
 
 (require test-engine/racket-tests)
 (test)
