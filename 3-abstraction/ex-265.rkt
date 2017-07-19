863e4b5413ea606ce32ee05ca72db5d502ae9a0d
bgusach <bgusach@gmail.com>
2017-07-19 19:15:19 +0200

    265

---
+++ 3-abstraction/ex-265.rkt
@@ -0,0 +1,18 @@
+#lang htdp/isl
+
+(require test-engine/racket-tests)
+    
+(check-expect
+  ((local 
+     ((define (f x)
+        (+ (* 4 (sqr x)) 3)
+        ))
+
+     ; -- IN --
+     f)
+   1)
+  7
+  )
+
+(test)
+
