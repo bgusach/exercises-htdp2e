27a7c69e4716c5194c7bf7ac1ff6c782dea731f5
bgusach <bgusach@gmail.com>
2017-07-19 19:31:50 +0200

    356 to 359

--- 4-intertwined-data/ex-352-to-359.rkt
+++ 4-intertwined-data/ex-352-to-359.rkt
@@ -178,15 +178,17 @@
 ; A BSL-fun-expr is one of:
 ; - Number
 ; - Symbol
-; - (make-add BSL-var-expr BSL-var-expr)
-; - (make-mul BSL-var-expr BSL-var-expr)
-; - (make-fn-app Symbol BSL-var-expr)
+; - (make-add BSL-var-expr BSL-fun-expr)
+; - (make-mul BSL-var-expr BSL-fun-expr)
+; - (make-fn-app Symbol BSL-fun-expr)
 ;
 ; Examples:
 ; - (k (+ 1 1)) -> 
 ;     (make-fn-app 'k (make-add 1 1))
+;
 ; - (* 5 (k (+ 1 1))) -> 
 ;     (make-mul 5 (make-fn-app 'k (make-add 1 1)))
+;
 ; - (* (i 5) (k (+ 1 1))) ->
 ;     (make-mul
 ;       (make-fn-app 'i 5)
@@ -205,28 +207,177 @@
     (add-one ,add1)
     ))
 
-; BSL-fun-expr AL -> Number
-(check-expect (eval-definition1 5 scope1) 5)
+(define add-5-name 'add-5)
+(define add-5-arg-name 'x)
+(define add-5-body (make-add 5 add-5-arg-name))
+
+; BSL-fun-expr Symbol Symbol BSL-fun-expr -> Number
+; Evaluates `ex` assuming there is only the function 
+; `fn-name` in the scope, with the argument `arg`,
+; and its body is the expression `body`.
+(check-expect (eval-definition1 1 add-5-name add-5-arg-name add-5-body) 1)
+(check-expect (eval-definition1 (make-add 2 3) add-5-name add-5-arg-name add-5-body) 5)
 (check-expect 
-  (eval-definition1 (make-fn-app 'add-one 'x) scope1) 
+  (eval-definition1 (make-add 2 (make-mul 2 2)) add-5-name add-5-arg-name add-5-body) 
   6
   )
 (check-expect 
-  (eval-definition1 
-    (make-mul 2 (make-fn-app 'add-one 'x))
-    scope1
-    ) 
-  12
+  (eval-definition1 (make-fn-app add-5-name 10) add-5-name add-5-arg-name add-5-body) 
+  15
   )
-(define (eval-definition1 ex da)
+(check-expect
+  (eval-definition1
+    (make-mul 10 (make-fn-app add-5-name 7))
+    add-5-name
+    add-5-arg-name
+    add-5-body
+    )
+  120
+  )
+(check-error (eval-definition1 'x add-5-name add-5-arg-name add-5-body))
+(check-error 
+  (eval-definition1 
+    (make-fn-app 'x 4)  ; <- function x is not defined in this context
+    add-5-name 
+    add-5-arg-name 
+    add-5-body
+    ))
+(check-expect
+  (eval-definition1
+    (make-fn-app add-5-name (make-fn-app add-5-name 7))
+    add-5-name
+    add-5-arg-name
+    add-5-body
+    )
+  17
+  )
+; NOTE: this test will end up in infinite recursion
+; (check-error 
+;   (eval-definition1
+;     (make-fn-app add-5-name 5) 
+;     add-5-name 
+;     add-5-arg-name 
+;     (make-fn-app add-5-name 5)
+;     ))
+
+(define (eval-definition1 ex fn-name arg-name body)
   (match
     ex
     [(? number?) ex]
-    [(? symbol?) (resolve-sym ex da)]
-    [(add l r) (+ (eval-definition1 l da) (eval-definition1 r da))]
-    [(mul l r) (* (eval-definition1 l da) (eval-definition1 r da))]
-    [(fn-app fn arg) ((resolve-sym fn da) (eval-definition1 arg da))]
+    [(? symbol?) (error "no free variables are allowed")]
+    [(add l r) 
+     (+ 
+       (eval-definition1 l fn-name arg-name body) 
+       (eval-definition1 r fn-name arg-name body)
+       )]
+    [(mul l r) 
+     (* 
+       (eval-definition1 l fn-name arg-name body) 
+       (eval-definition1 r fn-name arg-name body)
+       )]
+    [(fn-app name arg) 
+     (cond
+       [(not (symbol=? name fn-name)) (error "unknown function")]
+       [else
+         (eval-definition1 
+           (subst.v2
+             body 
+             arg-name 
+             (eval-definition1 arg fn-name arg-name body) 
+             ; NOTE: if the previous line was only `arg`,
+             ; it would work too, but I guess it would be
+             ; a mess when there are multiple nested
+             ; functions that share the same var names
+             )
+           fn-name 
+           arg-name 
+           body
+           )])]))
+
+; BSL-fun-expr Symbol Number -> BSL-fun-expr
+; Same as subst, but handles fn-app structures
+(define (subst.v2 ex sym val)
+  (match
+    ex
+    [(? number?) ex]
+    [(? symbol?) (if (symbol=? ex sym) val ex)]
+    [(add l r) (make-add (subst.v2 l sym val) (subst.v2 r sym val))]
+    [(mul l r) (make-mul (subst.v2 l sym val) (subst.v2 r sym val))]
+    [(fn-app fn-name body) 
+     (make-fn-app fn-name (subst.v2 body sym val))]
     ))
+
+; =================== End of exercise ==================
+
+
+
+
+; ==================== Exercise 358 ====================
+
+; ### Data Definitions
+
+(define-struct fn-def [name param body])
+; a BSL-fun-def is a structure:
+;   (make-fn-def Symbol Symbol BSL-fun-expr)
+; i.e.: a function called `name` with a unique
+;   parameter `param` and with function `body`
+;
+; Examples:
+(define (f x) (+ 3 x))  ; ->
+(define f-def (make-fn-def 'f 'x (make-add 3 'x)))
+
+(define (g y) (f (* 2 y))) 
+(define g-def (make-fn-def 'g 'y (make-fn-app 'f (make-mul 2 'y))))
+
+(define (h v) (+ (f v) (g v)))
+(define h-def
+  (make-fn-def 'h 'v (make-add (make-fn-app 'f 'v) (make-fn-app 'g 'v)))
+  )
+
+; A BSL-fun-def* is a [List-of BSL-fun-def]
+; Can be understood as a definitions area (or scope)
+; populated with function definitions.
+(define da-fgh (list f-def g-def h-def))
+
+; BSL-fun-def* Symbol -> [Either BSL-fun-def Error]
+; Given a definitions-area / scope and a function name
+; the proper function def is returned or an error if
+; not found
+(check-expect (lookup-def da-fgh 'g) g-def)
+(define (lookup-def da fn-name)
+  (match
+    da
+    ['() (error "function definition not found")]
+    [(cons head tail)
+      (if
+        (symbol=? (fn-def-name head) fn-name)
+        head
+        (lookup-def tail fn-name)
+        )]))
+
+; =================== End of exercise ==================
+
+
+
+
+; ==================== Exercise 359 ====================
+
+; BSL-fun-expr BSL-fun-def* -> Number
+(check-expect (eval-function* 1 da-fgh) 1)
+(check-expect (eval-function* (make-mul 3 4) da-fgh) 12)
+(check-expect (eval-function* (make-add 3 4) da-fgh) 7)
+(check-expect (eval-function* (make-fn-app 'f 3) da-fgh) 6)
+(define (eval-function* ex da)
+  (match
+    ex
+    [(? number?) ex]
+    [(? symbol?) (error "free variable?")]
+    [(mul x y) (* (eval-function* x da) (eval-function* y da))]
+    [(add x y) (+ (eval-function* x da) (eval-function* y da))]
+    [(fn-app fn-name arg)
+      (eval-function* arg da)
+      (lookup-def da fn-name)
+      ]))
 
 ; =================== End of exercise ==================
 
