021b5868866e116abdcca01e4235dcad9bec5d4a
bgusach <bgusach@gmail.com>
2017-07-31 22:43:42 +0200

    390

--- 4-intertwined-data/ex-387-to-390.rkt
+++ 4-intertwined-data/ex-387-to-390.rkt
@@ -146,5 +146,55 @@
 
 ; =================== End of exercise ==================
 
+
+
+
+; ==================== Exercise 390 ====================
+
+(define-struct branch [left right])
+
+; A Tree of Symbols (TOS) is one of:
+; - Symbol
+; - (make-branch TOS TOS)
+
+; A Direction is one of:
+; - 'left
+; - 'right
+
+; A Path is a [List-of Direction]
+
+
+; TOS Path -> [Either Direction Error]
+(check-expect (tree-pick 'a '()) 'a)
+(check-expect (tree-pick (make-branch 'a 'b) '(right)) 'b)
+(check-expect 
+  (tree-pick 
+    (make-branch 'a (make-branch 'b 'c)) 
+    '(right left)
+    )
+  'b
+  )
+(check-error 
+  (tree-pick (make-branch 'a 'b) '(right right right))
+  "too many directions"
+  )
+(check-error 
+  (tree-pick (make-branch 'a 'b) '())
+  "too few directions"
+  )
+(define (tree-pick tree lod)
+  (cond
+    [(and (symbol? tree) (empty? lod)) tree]
+    [(and (symbol? tree) (cons? lod)) (error "too many directions")]
+    [(and (branch? tree) (empty? lod)) (error "too few directions")]
+    [(and (branch? tree) (cons? lod)) 
+     (if
+       (symbol=? (first lod) 'left)
+       (tree-pick (branch-left tree) (rest lod))
+       (tree-pick (branch-right tree) (rest lod))
+       )]))
+
+; =================== End of exercise ==================
+
 (test)
 
